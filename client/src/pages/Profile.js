6f4d9bfbc4ded341df247e6dd055b58fcf563628
KyleAxley <kanderson7645@yahoo.com>
2022-09-29 23:29:37 -0400

    thought form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
-import { useQuery} from '@apollo/client';
-import { Navigate, useParams } from 'react-router-dom';
-import { QUERY_USER, QUERY_ME} from '../utils/queries';
-import Auth from '../utils/auth';
+import React from "react";
+import ThoughtList from "../components/ThoughtList";
+import ThoughtForm from "../components/ThoughtForm";
+import FriendList from "../components/FriendList";
+import { useQuery, useMutation } from "@apollo/client";
+import { ADD_FRIEND } from "../utils/mutations";
+import { Navigate, useParams } from "react-router-dom";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery( userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam }
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
 
   //navigate to personal profile page if username is the logged in user's
-  if(Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/profile" />;
   }
 
@@ -24,25 +27,45 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
-  if(!user?.username) {
+  if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in. 
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in.
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing  {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
-      
-        <div className="flex-row justify-space-between mb-3">
+
+      <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
@@ -53,6 +76,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
